cd10e4b46efb53176a24e859059a42ed3a357558
paoloziff <84146302+paoloziff@users.noreply.github.com>
2023-10-16 20:40:39 +0200

    Update chart.py

--- chart.py
+++ chart.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import font_manager
+import streamlit as st
+import base64
 
 
 def create_data(numbers):
@@ -45,3 +47,23 @@ def make_chart(numbers):
         spine.set_visible(False)
 
     return fig
+
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
+    page_bg_img = '''
+    <style>
+    body {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    opacity: 0.2;
+    }
+    </style>
+    ''' % bin_str
+    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
