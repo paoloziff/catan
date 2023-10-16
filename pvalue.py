4fe5c2d45ebf91f62f3a0e6027c8d6e43108367b
paoloziff <84146302+paoloziff@users.noreply.github.com>
2023-10-16 17:50:05 +0200

    Update pvalue.py

--- pvalue.py
+++ pvalue.py
@@ -1,3 +1,4 @@
+import base64
 from scipy.stats import chisquare
 
 
@@ -20,3 +21,15 @@ def get_p_value(outcomes):
     expected = expected_frequencies(n)
     chi2, p = chisquare(observed, expected)
     return p
+
+def autoplay_audio(file_path: str):
+    with open(file_path, "rb") as f:
+        data = f.read()
+        b64 = base64.b64encode(data).decode()
+        md = f"""
+            <audio controls autoplay="true">
+            <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+            </audio>
+            """
+        st.markdown(md, unsafe_allow_html=True)
+        
